d2c094804d5829cba45828699c6a3dea2140ca92
Devan Beitel <devan@devanb.us>
2020-11-24 00:27:20 -0500

    fix(i18n): remove domains list

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@ module.exports = {
   i18n: {
     locales: ["en-US"],
     defaultLocale: "en-US",
-    domains: [
-      {
-        domain: "w1-wgw.vercel.app",
-        defaultLocale: "en-US",
-      },
-    ],
   },
   images: {
     domains: ["images.ctfassets.net"],
